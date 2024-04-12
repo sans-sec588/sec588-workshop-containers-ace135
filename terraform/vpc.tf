d1898a850b932c496187a1177e596385335951db
Moses Frost <moses@moses.io>
2024-04-12 17:25:52 +0100

    Fixes for VPC

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,15 +8,6 @@ resource "aws_internet_gateway" "gateway" {
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
-}
-
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
 }
