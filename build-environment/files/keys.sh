ea650e687f34c2ba26c2cb475f2d479647a2298a
Moses Frost <moses@moses.io>
2024-04-14 22:02:53 +0100

    Fixes for the EC2 Server

--- build-environment/files/keys.sh
+++ build-environment/files/keys.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-echo "You think you found the plans to Aviata? Maybe you should stay tuned to sans.org/ace135 and find out what's next..." | sudo tee /etc/keys
+#!/bin/sh
+mkdir -p /opt/aviata/data
+echo "You think you found the plans to Aviata? Maybe you should stay tuned to sans.org/ace135 and find out what's next..." | sudo tee /opt/aviata/data/plans
