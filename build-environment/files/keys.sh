608f08fa83b165dc0029b6f36d268357be32d345
Moses Frost <moses@moses.io>
2024-04-14 19:57:59 +0100

    Fixes

--- build-environment/files/keys.sh
+++ build-environment/files/keys.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "You think you found the plans to Aviata? Maybe you should stay tuned to sans.org/ace135 and find out what's next..." | sudo tee /var/run/keys
+echo "You think you found the plans to Aviata? Maybe you should stay tuned to sans.org/ace135 and find out what's next..." | sudo tee /etc/keys
